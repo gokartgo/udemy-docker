9c21d2db26759bff4abbfc0dade8c6838f40ab74
gokartgo <gokart.007@hotmail.com>
2020-05-24 20:09:31 +0700

    finish 73

---
+++ secrets-sample-1/secrets-sample-1.sh
@@ -0,0 +1,16 @@
+# create secret from file txt
+docker secret create psql_user psql_user.txt
+# create secret from cmd
+echo "myDBpassWORD" | docker secret create psql_pass -
+
+docker secret ls
+
+docker secret inspect <secret name>
+# create service and set env from secret
+docker service create --name psql --secret psql_user --secret psql_pass -e POSTGRES_PASSWORD_FILE=/run/secrets/psql_pass -e POSTGRES_USER_FILE=/run/secrets/psql_user postgres
+# show where container run
+docker service ps <service name>
+# execute container
+docker exec -it <container name of service> bash
+# log status of container
+docker logs <container name>
